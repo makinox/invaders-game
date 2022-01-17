b2cd2dc64ae6c774dfb10f50251f9bc98fc8f4de
Jesus Bossa <x@jesusbossa.dev>
2022-01-16 21:31:03 -0500

    chore: ship added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import kaboom from 'kaboom';
+
+import spaceShip from '/assets/space-ship.png';
 import enemy from '/assets/enemy.png';
-import wall from '/assets/wall.png';
 import block from '/assets/block.png';
+import wall from '/assets/wall.png';
 
 kaboom();
 
 loadSprite('enemy', enemy);
 loadSprite('wall', wall);
 loadSprite('block', block);
+loadSprite('spaceShip', spaceShip);
 
 addLevel(
   [
@@ -31,3 +34,5 @@ addLevel(
     '&': () => [sprite('block'), scale(0.8)],
   }
 );
+
+const player = add([sprite('spaceShip'), pos(width() / 5, height() / 2.8)]);
