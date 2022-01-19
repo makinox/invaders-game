100e4369ba0c8015a19cc7257b31e54b24d5069d
Jesus Bossa <x@jesusbossa.dev>
2022-01-18 20:57:31 -0500

    chore: bullets & scores

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import kaboom from 'kaboom';
+import kaboom, { GameObj, SpriteComp, Vec2 } from 'kaboom';
 
 import spaceShip from '/assets/space-ship.png';
 import enemy from '/assets/enemy.png';
@@ -15,6 +15,7 @@ loadSprite('block', block);
 loadSprite('spaceShip', spaceShip);
 
 const MOVE_SPEED = 200;
+const BULLET_SPEED = 400;
 const ENEMY_SPEED = 100;
 let CURRENT_SPEED = 100;
 const LEVEL_DOWN = 50;
@@ -48,13 +49,29 @@ addLevel(
   }
 );
 
-const player = add([sprite('spaceShip'), pos(width() / 5, height() / 2.8)]);
+const player = add([sprite('spaceShip'), pos(width() / 5, height() / 2.8), area(), 'player']);
 keyDown('left', () => {
   player.move(-MOVE_SPEED, 0);
 });
 
 keyDown('right', () => {
   player.move(MOVE_SPEED, 0);
+});
+
+function spawnBullet(p: Vec2) {
+  add([rect(6, 18), pos(p), color(0.5, 0.5, 1), area(), 'bullet']);
+}
+
+keyPress('space', () => {
+  console.log('space');
+  spawnBullet(player.pos.add(29, -25));
+});
+
+action('bullet', (bullet) => {
+  bullet.move(0, -BULLET_SPEED);
+  if (bullet.pos.y < 0) {
+    destroy(bullet);
+  }
 });
 
 const score = add([
@@ -93,7 +110,6 @@ action('enemy', (enemy) => {
 
 onCollide('enemy', 'right-wall', () => {
   CURRENT_SPEED = -ENEMY_SPEED;
-  console.log('ee');
   every('enemy', (enemy) => {
     enemy.move(0, LEVEL_DOWN);
   });
@@ -104,4 +120,25 @@ onCollide('enemy', 'left-wall', () => {
   every('enemy', (enemy) => {
     enemy.move(0, LEVEL_DOWN);
   });
+});
+
+onCollide('enemy', 'player', () => {
+  // console.log('loose');
+  // go('lose', 100);
+});
+
+onCollide('bullet', 'enemy', (bullet, enemy) => {
+  destroy(bullet);
+  destroy(enemy);
+  shake(4);
+  const actualScore = score.value + 1;
+  score.value = actualScore;
+  score.text = actualScore.toString();
+});
+
+action('enemy', (enemy) => {
+  if (enemy.pos.y >= 12 * 22) {
+    console.log('loose');
+    // go('lose', 100);
+  }
 });
