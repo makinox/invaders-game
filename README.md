9f677fbfec4fcd277c062e987a2cc83676e5ab1f
Jesus Bossa <x@jesusbossa.dev>
2022-01-22 15:23:35 -0500

    chore: add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Invaders 👾
+
+Used:
+
+- Vanilla
+- Kaboom
+
+Link to [invaders!](https://invaders.jesusbossa.dev/)
+
+![Image of invaders](./assets/preview.png)
--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: {
@@ -11,6 +12,29 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Memory game',
       template: path.resolve(__dirname, 'src/template/index.html'),
+    }),
+    new WebpackPwaManifest({
+      name: 'Invaders game',
+      short_name: 'Invaders game',
+      description: 'Invaders game - Jesus bossa',
+      background_color: '#ffffff',
+      crossorigin: 'anonymous',
+      ios: true,
+      icons: [
+        {
+          src: path.resolve('assets/enemy.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+        {
+          src: path.resolve('assets/enemy.png'),
+          size: '1024x1024',
+        },
+        {
+          src: path.resolve('assets/enemy.png'),
+          size: '1024x1024',
+          purpose: 'maskable',
+        },
+      ],
     }),
   ],
   output: {
