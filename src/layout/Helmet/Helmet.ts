27e57f51e039d7366f7b954f04da62f98d5f8e9b
Jesus Bossa <x@jesusbossa.dev>
2022-01-22 15:57:04 -0500

    chore: change og images

--- src/layout/Helmet/Helmet.ts
+++ src/layout/Helmet/Helmet.ts
@@ -1,4 +1,5 @@
 import Icon from '/assets/enemy.png';
+import Preview from '/assets/preview.png';
 
 export default function Helmet() {
   const faviconElement = document.createElement('link');
@@ -7,11 +8,11 @@ export default function Helmet() {
 
   const imageFace = document.createElement('meta');
   imageFace.name = 'og:image';
-  imageFace.content = window.location.href.substring(0, window.location.href.length - 1) + Icon;
+  imageFace.content = window.location.href.substring(0, window.location.href.length - 1) + Preview;
 
   const imageTw = document.createElement('meta');
   imageTw.name = 'twitter:image';
-  imageTw.content = window.location.href.substring(0, window.location.href.length - 1) + Icon;
+  imageTw.content = window.location.href.substring(0, window.location.href.length - 1) + Preview;
 
   document.head.appendChild(faviconElement);
   document.head.appendChild(imageFace);
