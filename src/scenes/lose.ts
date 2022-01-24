56483efb0f4e24b906ece8864154af2f49fed644
Jesus Bossa <x@jesusbossa.dev>
2022-01-24 09:54:06 -0500

    feat: add play again

--- src/scenes/lose.ts
+++ src/scenes/lose.ts
@@ -1,9 +1,11 @@
 export default function lose() {
   return scene('lose', (args) => {
     add([text(`You loose: ${args}`), scale(2), pos(width() / 2.5, height() / 2)]);
-    // const continue = window.confirm('Play again?')
-    // if (continue) {
-    //   window.location.reload()
-    // }
+
+    setTimeout(() => {
+      if (window.confirm('Play again?')) {
+        window.location.reload();
+      }
+    }, 1000);
   });
 }
--- src/scenes/win.ts
+++ src/scenes/win.ts
@@ -1,5 +1,11 @@
 export default function win() {
   return scene('win', (args) => {
     add([text(`you win: ${args}`), scale(2), pos(width() / 2.5, height() / 2)]);
+
+    setTimeout(() => {
+      if (window.confirm('Play again?')) {
+        window.location.reload();
+      }
+    }, 1000);
   });
 }
